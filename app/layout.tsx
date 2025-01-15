597e61e8cae69abeb27cea1f905a5e6614adfaeb
vteodorodev <vteodorodev@gmail.com>
2025-01-15 15:32:29 +0000

    fixed issue with background image noise not showing in prod

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
-import type { Metadata } from 'next';
 import './globals.css';
 import Header from '@/components/header';
+
+import NoiseBackground from '@/public/img-noise-361x370.png';
+import Image from 'next/image';
 
 //TODO: Add description
 export const metadata = {
@@ -15,7 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      {/* TODO: Find way to put background in css. NextJS isn't locating the file */}
+      <body style={{ backgroundImage: `url(${NoiseBackground.src})` }}>
         <Header />
         <main>{children}</main>
         {/* <footer>© 2025 Vasco Teodoro • Built with NextJS</footer> */}
