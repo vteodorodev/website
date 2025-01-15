6ff848de79e6efa8ae52eab24c23aa0a711bde14
vteodorodev <vteodorodev@gmail.com>
2025-01-15 21:23:01 +0000

    made mockup portfolio page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'www.mathworks.com',
+      },
+      {
+        hostname: 'upload.wikimedia.org',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
